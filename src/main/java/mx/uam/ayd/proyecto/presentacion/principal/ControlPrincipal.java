a520e4ef1b01fcda199fc969d169c7d88f9d9a26
yael-afk643 <yael.mora2005@gmail.com>
2026-07-23 23:03:48 -0600

    agrege metodos

--- src/main/java/mx/uam/ayd/proyecto/presentacion/principal/ControlPrincipal.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/principal/ControlPrincipal.java
@@ -7,17 +7,18 @@ import org.springframework.stereotype.Component;
 
 import mx.uam.ayd.proyecto.presentacion.actualizarPrecioProductos.ControlActualizarPrecio;
 import mx.uam.ayd.proyecto.presentacion.agregarUsuario.ControlAgregarUsuario;
+import mx.uam.ayd.proyecto.presentacion.consultarInventario.ControlConsultarInventario;
 import mx.uam.ayd.proyecto.presentacion.devolucionProducto.ControlDevolucionProducto;
-import mx.uam.ayd.proyecto.presentacion.listarUsuarios.ControlListarUsuarios;
+import mx.uam.ayd.proyecto.presentacion.historialMovimientos.ControlHistorialMovimientos;
 import mx.uam.ayd.proyecto.presentacion.listarGrupos.ControlListarGrupos;
 import mx.uam.ayd.proyecto.presentacion.listarInventario.ControlListarInventario;
+import mx.uam.ayd.proyecto.presentacion.listarUsuarios.ControlListarUsuarios;
 import mx.uam.ayd.proyecto.presentacion.registrarMercancia.ControlRegistrarMercancia;
 
 /**
- * Esta clase lleva el flujo de control de la ventana principal
- * 
- * @author humbertocervantes
+ * Esta clase lleva el flujo de control de la ventana principal.
  *
+ * @author humbertocervantes
  */
 @Component
 public class ControlPrincipal {
@@ -25,22 +26,26 @@ public class ControlPrincipal {
     private final ControlAgregarUsuario controlAgregarUsuario;
     private final ControlListarUsuarios controlListarUsuarios;
     private final ControlListarGrupos controlListarGrupos;
-    private final VentanaPrincipal ventana;
     private final ControlListarInventario controlListarInventario;
     private final ControlRegistrarMercancia controlRegistrarMercancia;
     private final ControlActualizarPrecio controlActualizarPrecio;
     private final ControlDevolucionProducto controlDevolucion;
+    private final ControlConsultarInventario controlConsultarInventario;
+    private final ControlHistorialMovimientos controlHistorialMovimientos;
+    private final VentanaPrincipal ventana;
 
     @Autowired
     public ControlPrincipal(
-        ControlAgregarUsuario controlAgregarUsuario,
-        ControlListarUsuarios controlListarUsuarios,
-        ControlListarGrupos controlListarGrupos,
-        ControlListarInventario controlListarInventario,
-        ControlRegistrarMercancia controlRegistrarMercancia,
-        ControlActualizarPrecio controlActualizarPrecio, 
-        ControlDevolucionProducto controlDevolucion,
-        VentanaPrincipal ventana) {
+            ControlAgregarUsuario controlAgregarUsuario,
+            ControlListarUsuarios controlListarUsuarios,
+            ControlListarGrupos controlListarGrupos,
+            ControlListarInventario controlListarInventario,
+            ControlRegistrarMercancia controlRegistrarMercancia,
+            ControlActualizarPrecio controlActualizarPrecio,
+            ControlDevolucionProducto controlDevolucion,
+            ControlConsultarInventario controlConsultarInventario,
+            ControlHistorialMovimientos controlHistorialMovimientos,
+            VentanaPrincipal ventana) {
 
         this.controlAgregarUsuario = controlAgregarUsuario;
         this.controlListarUsuarios = controlListarUsuarios;
@@ -49,102 +54,87 @@ public class ControlPrincipal {
         this.controlRegistrarMercancia = controlRegistrarMercancia;
         this.controlActualizarPrecio = controlActualizarPrecio;
         this.controlDevolucion = controlDevolucion;
+        this.controlConsultarInventario = controlConsultarInventario;
+        this.controlHistorialMovimientos = controlHistorialMovimientos;
         this.ventana = ventana;
     }
-    
+
     /**
-     * Método que se ejecuta después de la construcción del bean
-     * y realiza la conexión bidireccional entre el control principal y la ventana principal
+     * Método que se ejecuta después de la construcción del bean y realiza
+     * la conexión entre el control principal y la ventana principal.
      */
     @PostConstruct
     public void init() {
         ventana.setControlPrincipal(this);
     }
-    
+
     /**
-     * Inicia el flujo de control de la ventana principal
-     * 
+     * Inicia el flujo de control de la ventana principal.
      */
     public void inicia() {
         ventana.muestra();
     }
 
     /**
-     * Método que arranca la historia de usuario "agregar usuario"
-     * 
+     * Arranca la historia de usuario "Agregar usuario".
      */
     public void agregarUsuario() {
         controlAgregarUsuario.inicia();
     }
-    
+
     /**
-     * Método que arranca la historia de usuario "listar usuarios"
-     * 
+     * Arranca la historia de usuario "Listar usuarios".
      */
     public void listarUsuarios() {
         controlListarUsuarios.inicia();
     }
 
     /**
-     * Método que arranca la historia de usuario "listar grupos"
-     * 
+     * Arranca la historia de usuario "Listar grupos".
      */
     public void listarGrupos() {
         controlListarGrupos.inicia();
     }
 
     /**
-     * Método que arranca la historia de usuario "listar inventario"
+     * Arranca la historia de usuario "Listar inventario".
      */
     public void listarInventario() {
         controlListarInventario.inicia();
     }
 
     /**
-     * Método que arranca la historia de usuario "Registrar mercancía".
+     * Arranca la historia de usuario "Registrar mercancía".
      */
     public void registrarMercancia() {
         controlRegistrarMercancia.inicia();
     }
 
     /**
-     * Método que arranca la historia de usuario "Actualizar precio".
+     * Arranca la historia de usuario "Actualizar precio".
      */
     public void actualizarPrecio() {
-        controlActualizarPrecio.inicia(); // Al usarlo, desaparece el warning de atributo no usado
+        controlActualizarPrecio.inicia();
     }
 
     /**
-     * Método que arranca la historia de usuario "Devolución de producto".
+     * Arranca la historia de usuario "Devolución de producto".
      */
     public void devolucionProducto() {
-        controlDevolucion.inicia(); // Al usarlo, desaparece el warning de atributo no usado
+        controlDevolucion.inicia();
     }
-}
-@Autowired
-public ControlPrincipal(
-        ControlAgregarUsuario controlAgregarUsuario,
-        ControlListarUsuarios controlListarUsuarios,
-        ControlListarGrupos controlListarGrupos,
-        ControlListarInventario controlListarInventario,
-        ControlRegistrarMercancia controlRegistrarMercancia,
-        ControlConsultarInventario controlConsultarInventario,
-        ControlHistorialMovimientos controlHistorialMovimientos,
-        VentanaPrincipal ventana) {
-            this.controlConsultarInventario = controlConsultarInventario;
-this.controlHistorialMovimientos = controlHistorialMovimientos;
-/**
- * Método que arranca la historia de usuario
- * "Consultar Inventario".
- */
-public void consultarInventario() {
-    controlConsultarInventario.inicia();
-}
 
-/**
- * Método que arranca la historia de usuario
- * "Historial de Movimientos".
- */
-public void historialMovimientos() {
-    controlHistorialMovimientos.inicia();
+    /**
+     * Arranca la historia de usuario "Consultar inventario".
+     */
+    public void consultarInventario() {
+        controlConsultarInventario.inicia();
+    }
+
+    /**
+     * Arranca la historia de usuario "Historial de movimientos".
+     */
+    public void historialMovimientos() {
+        controlHistorialMovimientos.inicia();
+    }
 }
