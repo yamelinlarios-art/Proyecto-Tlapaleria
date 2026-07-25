e0b0a8b5fa062e917651e33d4bd8dee717acd177
Dyd1er <kevindydierlopezflores@gmail.com>
2026-07-24 20:56:42 -0600

    Volvi a la normalidad todo

--- src/main/java/mx/uam/ayd/proyecto/presentacion/registroVenta/ControlRegistroVenta.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/registroVenta/ControlRegistroVenta.java
@@ -96,3 +96,6 @@ public class ControlRegistroVenta {
         ventanaCarrito.setVisible(false);
     }
 }
+
+
+
--- src/main/java/mx/uam/ayd/proyecto/presentacion/registroVenta/VentanaCarrito.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/registroVenta/VentanaCarrito.java
@@ -21,7 +21,6 @@ import javafx.stage.Stage;
 import mx.uam.ayd.proyecto.negocio.modelo.DescripcionVenta;
 
 
-
 @Component
 public class VentanaCarrito {
 
